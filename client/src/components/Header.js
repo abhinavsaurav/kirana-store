129c8756d5b6e6217fa5f9d1ba55c4dff34eb533
root <root@Abhinav.localdomain>
2021-08-13 05:02:07 +0530

    Intial Commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () =>{
+    return (
+        <header>
+            <div>
+                logo
+            </div>
+            <div>
+                name
+            </div>
+            <div>
+                searchbar
+            </div>
+            <div>
+                account/signin/signout
+            </div>
+            <div>
+                cart
+            </div>
+        </header>
+    );
+};
+
+export default Header;
