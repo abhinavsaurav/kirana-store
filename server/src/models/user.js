0848e97a0b1d98600ce2082464e9168749629f15
Abhinav Saurav <abhinavsaurav1@gmail.com>
2022-01-04 17:57:04 +0530

    model created

---
+++ server/src/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			trim: true,
+			lowercase: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 8,
+		},
+		isAdmin: {
+			type: Boolean,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
