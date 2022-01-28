8f64ae69b114bd5aec34f103d731e356d9ad142e
Abhinav Saurav <abhinavsaurav1@gmail.com>
2022-01-29 04:38:32 +0530

    form validation and styling

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -4,11 +4,12 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { login } from '../../store/authActions';
 import { authDefaultActions } from '../../store/authSlice';
-import InputField from '../UI/input/InputField';
-import Button from '../UI/button/Button';
 import { ERROR_LOGIN_EMAIL, ERROR_LOGIN_SUBMIT } from '../../data/constants';
 
+import Button from '../UI/button/Button';
 import Spinner from '../UI/spinner/Spinner';
+import InputField from '../UI/input/InputField';
+
 import classes from './Login.module.scss';
 
 // Added notes below the export line
@@ -21,9 +22,9 @@ const Login = (props) => {
 	const dispatch = useDispatch();
 
 	// auth selectors
-	// const loginStatus = useSelector((state) => state.auth.status);
+	const loginStatus = useSelector((state) => state.auth.status);
 	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-	// const apiError = useSelector((state) => state.auth.error);
+	const apiError = useSelector((state) => state.auth.error);
 
 	// useEffect(() => {
 	// 	setErrorMessage(apiError);
@@ -35,13 +36,13 @@ const Login = (props) => {
 		 * TODO : username and password secure and authentication
 		 */
 		const temp = e.target.value;
-		const minPassLength = 8;
-		const maxPassLength = 16;
-		if (!(temp.length >= minPassLength) || !(temp.length <= maxPassLength)) {
-			setHasError(true);
-		} else {
-			setHasError(false);
-		}
+		// const minPassLength = 8;
+		// const maxPassLength = 16;
+		// if (!(temp.length >= minPassLength) || !(temp.length <= maxPassLength)) {
+		// 	setHasError(true);
+		// } else {
+		// 	setHasError(false);
+		// }
 
 		// TODO: This should be checked in handlesubmit form or not or i can create a
 		// checkbox indicating but this is n
@@ -50,15 +51,30 @@ const Login = (props) => {
 	};
 
 	const validateForm = (username, password) => {
-		if (hasError) {
-			setErrorMessage(ERROR_LOGIN_SUBMIT);
+		// check the end of the regex
+		const usernameRE =
+			/^[a-zA-Z][a-zA-Z0-9_-]{4,}@[a-zA-Z0-9]{2,}\.(com|in|[a-zA-Z]{2,3})/g;
+
+		console.log(usernameRE.test(username));
+		if (!usernameRE.test(username)) {
 			return false;
 		}
+
+		// For password the only test at login we are doing is checking the length
+		// The other test we must be doing should be done during the signup page
+		if (password.length < 8 && password.length > 20) {
+			return false;
+		}
+		// if (hasError) {
+		// 	return false;
+		// }
+
+		return true;
 	};
 
 	const handleFormSubmit = async (e) => {
 		e.preventDefault();
-		// await dispatch(authDefaultActions.resetAuthError());
+		await dispatch(authDefaultActions.resetAuthError());
 		setErrorMessage('');
 
 		/**
@@ -67,18 +83,26 @@ const Login = (props) => {
 		 *
 		 */
 		if (!validateForm(email, pass)) {
+			setHasError(true);
+			setErrorMessage(ERROR_LOGIN_SUBMIT);
+			return false;
 		}
 
 		await dispatch(login({ email, pass }));
-		return;
+
+		// return;
 		// const temp = pass;
 	};
 
 	const loginError = function () {
 		return (
-			<div className={classes.error}>
-				<div className={classes.message}>
-					{errorMessage || 'Something is not right. Please try logging again!'}
+			<div className={classes['error-wrapper']}>
+				<div className={classes.error}>
+					<div className={classes.message}>
+						{apiError ||
+							errorMessage ||
+							'Something is not right. Please try logging again!'}
+					</div>
 				</div>
 			</div>
 		);
@@ -86,59 +110,67 @@ const Login = (props) => {
 
 	return (
 		<div className={classes['container']}>
-			{hasError ? loginError() : ''}
-			<form onSubmit={handleFormSubmit} className={classes.form}>
-				<h1>Login</h1>
-				<div className={classes['form-container']}>
-					<div className={classes.uname}>
-						<label htmlFor="uname">Email </label>
-						<label>:</label>
-						<InputField
-							type="text"
-							id="uname"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</div>
-					<div className={classes.pass}>
-						<label className={hasError === true ? 'error' : ''} htmlFor="pass">
-							Password
-						</label>
-						<label>:</label>
-						<InputField
-							type="password"
-							id="pass"
-							value={pass}
-							onChange={validatePassword}
-						/>
-					</div>
-				</div>
-				<div className={classes['btn-container']}>
-					<div className={classes['submit-btn']}>
-						<Button
-							type="submit"
-							// onClick={testFunction}
-							// disabled={hasError === true ? true : false}
-							// className={classes['submit-btn']} to override the default style we can specify it
-						>
-							Login
-						</Button>
-					</div>
-				</div>
-				<div className={classes['link-cont']}>
-					<NavLink to="/user-register">New to kirana?</NavLink>
-					&nbsp;&nbsp;
-					<NavLink to="/reset-password">Forgot Password?</NavLink>
-				</div>
+			<div className={classes['login-container']}>
+				{hasError ? loginError() : ''}
 
-				<div className={classes.check}>
-					<div>
-						<td>Is Authenticated:</td>
-						<td>{isAuthenticated.toString()}</td>
-					</div>
+				<div className={classes['login-wrapper']}>
+					<form onSubmit={handleFormSubmit} className={classes.form}>
+						<h1>Login</h1>
+						<div className={classes['form-container']}>
+							<div className={classes.uname}>
+								<label htmlFor="uname">Email </label>
+								<label>:</label>
+								<InputField
+									type="text"
+									id="uname"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</div>
+							<div className={classes.pass}>
+								<label
+									className={hasError === true ? 'error' : ''}
+									htmlFor="pass"
+								>
+									Password
+								</label>
+								<label>:</label>
+								<InputField
+									type="password"
+									id="pass"
+									value={pass}
+									onChange={validatePassword}
+								/>
+							</div>
+						</div>
+						<div className={classes['btn-container']}>
+							<div className={classes['submit-btn']}>
+								<Button
+									type="submit"
+									// onClick={testFunction}
+									// disabled={hasError === true ? true : false}
+									// className={classes['submit-btn']} to override the default style we can specify it
+								>
+									Login
+								</Button>
+							</div>
+						</div>
+						<div className={classes['link-cont']}>
+							<NavLink to="/user-register">New to kirana?</NavLink>
+							&nbsp;&nbsp;
+							<NavLink to="/reset-password">Forgot Password?</NavLink>
+						</div>
+
+						<div className={classes.check}>
+							<div>
+								<td>Is Authenticated:</td>
+								<td>{isAuthenticated.toString()}</td>
+							</div>
+						</div>
+						{/* {loginStatus === 'loading' ? <Spinner /> : ''} */}
+					</form>
 				</div>
-				{/* {loginStatus === 'loading' ? <Spinner /> : ''} */}
-			</form>
+			</div>
 		</div>
 	);
 };
