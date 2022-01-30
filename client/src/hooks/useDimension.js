4e0904a85e066165d1e8db02417a04fd14a792a4
Abhinav Saurav <abhinavsaurav1@gmail.com>
2022-01-31 03:18:45 +0530

    added debounce mechanism on useDimension custom hook

--- client/src/hooks/useDimension.js
+++ client/src/hooks/useDimension.js
@@ -3,26 +3,39 @@ import { useState, useEffect } from 'react';
 const useDimension = () => {
 	let width = window.innerWidth;
 	let height = window.innerHeight;
-	const [dimension, setDimension] = useState([width, height]);
+	const [dimension, setDimension] = useState({ width, height });
 
 	const resizeMe = (e) => {
 		const w = window.innerWidth - 250;
 		const h = window.innerHeight;
-		setDimension([w, h]);
+		setDimension({ width: w, height: h });
 	};
 
 	/**
+	 *
+	 * UPDATE:ADDED DEBOUNCE BELOW
 	 * Need to add a debounce mechanism for performance reason
 	 * that is a set timeout and cleartimeou to clear a even after
 	 * a specified time
 	 *
 	 */
+
+	const debounce = (func, delay = 500) => {
+		let timeoutId;
+
+		return function () {
+			clearTimeout(timeoutId);
+			timeoutId = setTimeout(() => {
+				func.apply(this, arguments);
+			}, delay);
+		};
+	};
+
 	useEffect(() => {
-		window.addEventListener('resize', resizeMe);
-		resizeMe();
+		window.addEventListener('resize', debounce(resizeMe, 500));
 
 		return () => window.removeEventListener('resize', resizeMe);
-	}, []);
+	}, [window.innerWidth, window.innerHeight]);
 
 	return dimension;
 };
