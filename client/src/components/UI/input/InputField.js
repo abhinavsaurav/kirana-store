b938775fd5b1ce62d8278bdc1352ef567a038715
Abhinav Saurav <abhinavsaurav1@gmail.com>
2022-02-05 04:03:50 +0530

    New type added to include label

--- client/src/components/UI/input/InputField.js
+++ client/src/components/UI/input/InputField.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import classes from './InputField.module.scss';
 
-const InputField = ({ id, type, value, onChange, placeholder, ...props }) => {
+const InputField = ({
+	id,
+	type,
+	label,
+	variant,
+	value,
+	onChange,
+	placeholder,
+	...props
+}) => {
+	// console.log(type);
+
+	if (parseInt(variant) === 1) {
+		return (
+			<div className={classes.variant1}>
+				<label htmlFor={id}>{label}</label>
+				<input
+					id={id}
+					type={type}
+					value={value}
+					onChange={onChange}
+					placeholder={placeholder ?? null}
+					{...props}
+				/>
+			</div>
+		);
+	}
+
 	return (
 		<React.Fragment>
 			<input
