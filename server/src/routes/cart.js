36911caf868199a49749c3e1d74cdb7bacb2f7e2
Abhinav Saurav <abhinavsaurav1@gmail.com>
2022-02-11 02:30:22 +0530

    cart by id route created but needs to still be configured to only used for admins

--- server/src/routes/cart.js
+++ server/src/routes/cart.js
@@ -5,6 +5,7 @@ const Cart = require('../models/cart');
 
 const router = express.Router();
 
+// For getting the cart linked to a user
 router.get('/me', authMiddleware, cartMiddleware, (req, res, next) => {
 	try {
 		res.send(req.cart);
@@ -13,6 +14,7 @@ router.get('/me', authMiddleware, cartMiddleware, (req, res, next) => {
 	}
 });
 
+// For performing update request on the cart
 router.patch('/me', authMiddleware, cartMiddleware, async (req, res, next) => {
 	const allowedUpdates = ['cartItems', 'totalAmount'];
 	const updateKeys = Object.keys(req.body);
@@ -27,6 +29,31 @@ router.patch('/me', authMiddleware, cartMiddleware, async (req, res, next) => {
 		await req.cart.save();
 
 		res.send(req.cart);
+	} catch (err) {
+		req.status(401);
+		next(err);
+	}
+});
+
+// TODO Probably will be used by the ADMIN to see the cart items of a user
+// BUt currently its just returning the cart data based off of the id
+router.get('/:id', authMiddleware, async (req, res, next) => {
+	try {
+		// console.log(req.params.id);
+		if (req.params.id.length !== 24 || !/[0-9a-fA-F]{24}/.test(req.params.id)) {
+			res.status(400);
+			throw new Error('Invalid Cart Id');
+			// return next();
+		}
+
+		const _id = req.params.id;
+		const cart = await Cart.findOne({ _id });
+
+		if (!cart) {
+			throw new Error('Cart not found');
+		}
+
+		res.send(cart);
 	} catch (err) {
 		next(err);
 	}
